62ed25c66216e4e19e9f8dda30b23b6d22e9d9aa
Willian Batista <willian.alves.b15@gmail.com>
2023-10-24 17:11:38 -0300

    att1 finalizada

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,16 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int qtsAtividades = scan.nextInt();
+    for (int i = 1; i <= qtsAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String atividade = scan.next();
+      System.out.println("Digite o peso da atividade " + i + ":");
+      String pesoAtividade = scan.next();
+    }
+
+    scan.close();
   }
 }
